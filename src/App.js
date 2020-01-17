0c77a9208d17bf7cb301954ec2ad24bc7f823ac9
fiopwk <flopwiki@gmail.com>
2020-01-17 14:45:43 -0500

    changed console.log(err.message) to console.err(err)

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             .then(res => {
                 setData(res.data);
                 // console.log(res.data)
-            }).catch(err => {console.log(err.message)})
+            }).catch(err => {console.error(err)})
     }, []);
 
 
